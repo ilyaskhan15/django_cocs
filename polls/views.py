823f30ce4f72bf31f9ff015c06555a59430d756c
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2024-09-27 15:50:38 +0500

    do some changes in two files urls and view

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Hello! world, i am at the index of siteporject')
 
 # Create your views here.
