20fffbb3d0fdf7f121269d661d1e71afe3a71ed1
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2024-09-29 15:44:33 +0500

    add some django code to files

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Hello! world, i am at the index of siteporject')
 
+def detail(request, question_id):
+    return HttpResponse("you're  looking at a question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 # Create your views here.
